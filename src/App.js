ee0ea1e07dea616b6453563e5852123bb3747b0b
nebyukelkay <149935010+nebyukelkay@users.noreply.github.com>
2024-06-18 13:19:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 
-import Header from "./Component/Header/Header";
-import Carousel from "./Component/Caruosel/Carousel";
-import Category from "./Component/Category/category";
-import Product from "./Component/Products/Product";
+
 import './index.css'
 import Routing from "./Router";
 import { useContext, useEffect } from "react";
